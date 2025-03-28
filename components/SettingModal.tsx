040d59a8e65beaa065e3faacf4c9c78cff2e5ff7
erfan <erfan74sh@gmail.com>
2025-03-28 21:15:25 +0330

    encapsulate setting modal and its functionallity

--- components/SettingModal.tsx
+++ components/SettingModal.tsx
@@ -1,10 +1,11 @@
-import { View, Text, Modal } from "react-native";
+import { View, Text, Modal, TextInput, Button } from "react-native";
 import React from "react";
-
+import useConfigStore from "@/stores/useConfigStore";
+import { PomodoroPattern } from "@/stores/useConfigStore";
 interface SettingModalProps {
 	visible: boolean;
 	onRequestClose: () => void;
-	children: any;
+	children?: any;
 }
 
 const SettingModal = ({
@@ -12,7 +13,38 @@ const SettingModal = ({
 	onRequestClose,
 	children,
 }: SettingModalProps) => {
-	console.log({ visible });
+	const pomodoroPattern = useConfigStore((state) => state.pomodoroPattern);
+	const setPomodoroPattern = useConfigStore(
+		(state) => state.setPomodoroPattern
+	);
+
+	const [pomodoroPatternInput, setPomodoroPatternInput] = React.useState({
+		focusTime: pomodoroPattern.focusTime,
+		shortBreakTime: pomodoroPattern.shortBreakTime,
+		longBreakTime: pomodoroPattern.longBreakTime,
+		intervals: pomodoroPattern.intervals,
+	});
+
+	const onPomodoroPatternChange = (
+		keyToChange: keyof PomodoroPattern,
+		newVal: string
+	) => {
+		let newFormattedValue = Number(newVal);
+		if (keyToChange !== "intervals") {
+			newFormattedValue = newFormattedValue;
+			// newFormattedValue = newFormattedValue * 60 * 1000;
+		}
+		setPomodoroPatternInput((prevPattern) => ({
+			...prevPattern,
+			[keyToChange]: newFormattedValue,
+		}));
+	};
+
+	const onSaveChanges = () => {
+		setPomodoroPattern(pomodoroPatternInput);
+		onRequestClose();
+	};
+
 	return (
 		<Modal
 			animationType="slide"
@@ -21,7 +53,59 @@ const SettingModal = ({
 			onRequestClose={onRequestClose}
 		>
 			<View className="bg-gray-200 flex-1 m-10 p-5 rounded-2xl ">
-				{children}
+				<View className=" gap-y-2">
+					<View className="flex-row justify-between items-center">
+						<Text>Focus Time</Text>
+						<TextInput
+							className="inline-inputs"
+							keyboardType="numeric"
+							// placeholder="Focus time"
+							value={pomodoroPatternInput.focusTime.toString()}
+							onChangeText={(text) =>
+								onPomodoroPatternChange("focusTime", text)
+							}
+						/>
+					</View>
+					<View className="flex-row justify-between items-center">
+						<Text>Short break time</Text>
+						<TextInput
+							className="inline-inputs"
+							keyboardType="numeric"
+							placeholder="Short break time"
+							value={pomodoroPatternInput.shortBreakTime.toString()}
+							onChangeText={(text) =>
+								onPomodoroPatternChange("shortBreakTime", text)
+							}
+						/>
+					</View>
+					<View className="flex-row justify-between items-center">
+						<Text>Long break time</Text>
+						<TextInput
+							className="inline-inputs"
+							keyboardType="numeric"
+							placeholder="Long break time"
+							value={pomodoroPatternInput.longBreakTime.toString()}
+							onChangeText={(text) =>
+								onPomodoroPatternChange("longBreakTime", text)
+							}
+						/>
+					</View>
+					<View className="flex-row justify-between items-center">
+						<Text>Intervals</Text>
+						<TextInput
+							className="inline-inputs"
+							keyboardType="numeric"
+							placeholder="intervals"
+							value={pomodoroPatternInput.intervals.toString()}
+							onChangeText={(text) =>
+								onPomodoroPatternChange("intervals", text)
+							}
+						/>
+					</View>
+				</View>
+				<View className="mt-4">
+					<Button onPress={onSaveChanges} title="Save changes" />
+				</View>
 			</View>
 		</Modal>
 	);
